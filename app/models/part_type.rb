501d0cb08ca91b7f284b5c970d782de6006fa839
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-27 12:36:22 +0900

    refs #104 Modelを定義すること。バリデーション書くこと。 - validations

--- app/models/part_type.rb
+++ app/models/part_type.rb
@@ -1,3 +1,7 @@
 class PartType < ActiveRecord::Base
   attr_accessible :type_name
+
+  validates :type_name,
+    presence: true,
+    uniqueness: true
 end
--- spec/models/part_type_spec.rb
+++ spec/models/part_type_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe PartType do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation:' do
+
+    before(:all) do
+      PartType.create!(type_name: 'promoter')
+    end
+
+    before(:each) do
+      @type = PartType.new(type_name: 'terminator')
+    end
+
+    context 'with valid information' do
+      it 'should be valid' do
+        @type.should be_valid
+      end
+    end
+
+    context 'without :type_name' do
+      it 'should not be valid' do
+        @type.type_name = nil
+        @type.should_not be_valid
+      end
+    end
+
+    context ':type_name is not unique' do
+      it 'should not be valid' do
+        @type.type_name = 'promoter'
+        @type.should_not be_valid
+      end
+    end
+
+  end
 end
