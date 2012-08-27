f93d085a9bded8b1b51617711df6898c7d06f4d2
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-27 12:31:10 +0900

    refs #102 Modelを定義すること。バリデーション書くこと。 - validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,7 @@
 class Team < ActiveRecord::Base
   attr_accessible :team_name
+
+  validates :team_name,
+    presence: true,
+    uniqueness: true
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Team do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation:' do
+
+    before(:all) do
+      Team.create!(team_name: 'MIT')
+    end
+
+    before(:each) do
+      @team = Team.new(team_name: 'HokkaidoU_Japan')
+    end
+
+    context 'with valid information' do
+      it 'should be valid' do
+        @team.should be_valid
+      end
+    end
+
+    context 'without :team_name' do
+      it 'should not be valid' do
+        @team.team_name = nil
+        @team.should_not be_valid
+      end
+    end
+
+    context ':team_name is not unique' do
+      it 'should not be valid' do
+        @team.team_name = 'MIT'
+        @team.should_not be_valid
+      end
+    end
+
+  end
 end
