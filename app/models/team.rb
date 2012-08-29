b0a6ad52dafa8c5bd5806bba7af8154ae8a7faeb
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-29 10:18:50 +0900

    refs #109 Modelを定義すること。バリデーション書くこと。- validations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
   has_many :parts
+  has_many :users
   attr_accessible :team_name
 
   validates :team_name,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,39 @@
 class User < ActiveRecord::Base
-  attr_accessible :password_digest, :team_id, :user_auth_token, :user_email, :user_name, :user_status
+  belongs_to :team
+  has_secure_password
+  attr_accessible :team_id, :user_email, :user_name, :password, :password_confirmation
+
+  validates :team_id,
+    presence: { message: 'Select a team.' }
+
+  validates :team,
+    presence: { message: 'The team you selected is not existent.', if: :team_id? }
+
+  validates :user_name,
+    presence: { message: 'Enter username.' },
+    length: { allow_blank: true, minimum: 3, maximum: 10 },
+    uniqueness: { allow_blank: true, message: 'The username you entered is already taken. Enter unique one.' }
+
+  validates :user_email,
+    presence: { message: 'Enter Email address.' },
+    format: { allow_blank: true, with: /^[^@]{,50}@[a-zA-Z0-9\.\-_]{3,30}$/, message: 'Email has invalid format.' },
+    uniqueness: { allow_blank: true, message: 'The Email address you entered is already taken. Enter unique one.' }
+
+  validates :user_auth_token,
+    presence: true,
+    uniqueness: { allow_blank: true }
+
+  validates :user_status,
+    presence: true,
+    inclusion: { allow_blank: true, in: [0, 1, 2] }
+
+  validates :password,
+    presence: { on: :create, message: 'Enter password.' },
+    confirmation: { on: :create, message: 'password confirmation does not match.' }
+
+  def generate_auth_token
+    begin
+      self.user_auth_token = SecureRandom.urlsafe_base64(20)
+    end while User.exists?(user_auth_token: self.user_auth_token)
+  end
 end
