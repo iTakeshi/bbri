6dd5dfea57f1e29d9480e54521648e14e8ced2ca
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-10-02 20:47:28 +0900

    ranking by review count

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,5 +2,10 @@ class RankingController < ApplicationController
 
   # GET /
   def index
+    @hottest_parts = Array.new
+    Review.find(:all).group_by{|r| r.part_id}.each do |k, v|
+      @hottest_parts << [Part.find(k).part_identifier, v.count]
+    end
+    @hottest_parts.sort!{|a, b| b[1] <=> a[1]}
   end
 end
